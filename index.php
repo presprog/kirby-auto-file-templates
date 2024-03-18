89837fc7ec3c3561b65f3041b2d0cf2eefccf867
Benedict Massolle <bm@presentprogressive.de>
2024-03-18 13:50:00 +0100

    Define options in plugin definition file

--- index.php
+++ index.php
@@ -9,6 +9,10 @@ use PresProg\AutoFileTemplates\PluginOptions;
 @include_once __DIR__ . '/helpers.php';
 
 App::plugin('presprog/auto-file-templates', [
+    'options' => [
+        'autoAssign' => true,
+        'templates' => [],
+    ],
     'hooks' => [
         'file-create:after' => function (File $file) {
             $options = PluginOptions::createFromOptions(kirby()->options());
