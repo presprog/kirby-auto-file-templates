ed50cb01a0ebb2dcb76a99d1203b09158b7ee285
Benedict Massolle <bm@presentprogressive.de>
2024-03-19 09:55:37 +0100

    Remove unnecessary helpers file

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ use PresProg\AutoFileTemplates\AutoFileTemplates;
 use PresProg\AutoFileTemplates\PluginOptions;
 
 @include_once __DIR__ . '/vendor/autoload.php';
-@include_once __DIR__ . '/helpers.php';
 
 App::plugin('presprog/auto-file-templates', [
     'options' => [
